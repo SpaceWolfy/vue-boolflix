bbd2ce9798346fbb4dbdc35356cac213ae48f8df
Samuel Scozzari <s_scozzari@libero.it>
2022-02-03 12:11:55 +0100

    New component + base structure created

---
+++ src/components/MainSection.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="main">
+    <card-template />
+  </div>
+</template>
+
+<script>
+import CardTemplate from "./CardTemplate.vue";
+export default {
+  components: { CardTemplate },
+};
+</script>
+
+<style lang="scss" scoped></style>
