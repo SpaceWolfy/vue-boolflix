3daac6f54a1a330ffaf8fbbe0d06b6ec43f2c9d0
Samuel Scozzari <s_scozzari@libero.it>
2022-02-03 12:53:31 +0100

    added card structure sample, display info on hover

--- src/components/CardTemplate.vue
+++ src/components/CardTemplate.vue
@@ -1,9 +1,53 @@
 <template>
-  <div class="card-box"></div>
+  <div class="card-box">
+    <img
+      src="https://ssl-static.libsyn.com/p/assets/0/3/f/4/03f4dc2f16cc5d19/Back_to_the_Future.jpg"
+      alt="Movie poster"
+    />
+
+    <div class="info">
+      <div>ciao</div>
+      <div>ciao</div>
+      <div>ciao</div>
+      <div>ciao</div>
+      <div>ciao</div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return { isHovering: false };
+  },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.card-box {
+  height: 350px;
+  width: 240px;
+
+  img {
+    height: 100%;
+    width: 100%;
+  }
+
+  .info {
+    display: none;
+    color: white;
+  }
+
+  &:hover {
+    background-color: black;
+
+    img {
+      display: none;
+    }
+
+    .info {
+      display: block;
+    }
+  }
+}
+</style>
