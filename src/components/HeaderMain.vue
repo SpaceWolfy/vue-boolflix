1d6b1c0464b254946e6dd225413d3cdbb0ef7c08
Samuel Scozzari <s_scozzari@libero.it>
2022-02-03 12:26:19 +0100

    basic structure of the component created

--- src/components/HeaderMain.vue
+++ src/components/HeaderMain.vue
@@ -1,9 +1,28 @@
 <template>
-  <header></header>
+  <header>
+    <div class="title">BOOLFLIX</div>
+    <input class="search-bar" type="text" />
+  </header>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  padding: 30px 25px 15px 25px;
+  background-color: black;
+
+  .title {
+    color: red;
+    font-size: 28px;
+  }
+
+  .search-bar {
+    height: 28px;
+  }
+}
+</style>
